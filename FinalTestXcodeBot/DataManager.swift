68ea70bbc2b8403289b28b65b1e6ae8ff929464d
wimItanudjaja <tanudjaja@live.com>
2020-01-24 13:30:46 -0500

    WimITan commit

---
+++ FinalTestXcodeBot/DataManager.swift
@@ -0,0 +1,16 @@
+//
+//  DataManager.swift
+//  FinalTestXcodeBot
+//
+//  Created by Sean Rada on 1/24/20.
+//  Copyright © 2020 Rigil. All rights reserved.
+//
+
+import UIKit
+import KeychainAccess
+
+class DataManager: NSObject {
+    
+    var keychain = Keychain.init(service: "com.rigil.testBotTest23232323")
+
+}
