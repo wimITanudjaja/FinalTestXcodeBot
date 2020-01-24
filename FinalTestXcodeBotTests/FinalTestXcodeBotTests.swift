5d17b2a2020265cc3090ee4eb92d3905ece96a69
wimItanudjaja <tanudjaja@live.com>
2020-01-24 13:24:48 -0500

    Initial Commit

---
+++ FinalTestXcodeBotTests/FinalTestXcodeBotTests.swift
@@ -0,0 +1,34 @@
+//
+//  FinalTestXcodeBotTests.swift
+//  FinalTestXcodeBotTests
+//
+//  Created by Sean Rada on 1/24/20.
+//  Copyright © 2020 Rigil. All rights reserved.
+//
+
+import XCTest
+@testable import FinalTestXcodeBot
+
+class FinalTestXcodeBotTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
